2e18d6a6809730994df66e7a13472aea741e2eb7
Omar Garcia <omar.anibal.garcia@est.fib.upc.edu>
2018-04-08 19:37:16 +0200

    Max_lem = 10

--- Scene.h
+++ Scene.h
@@ -49,7 +49,7 @@ private:
 	float currentTime;
 	glm::mat4 projection;
 	int lastLemmingGenTime;
-	int const MAX_LEMMINGS = 1;
+	int const MAX_LEMMINGS = 10;
 	int lemmingCount = 0;
 	std::vector<Lemming*> lemmings;
 	string selectedHab;
